6d0c28640f09a4c939e797b2dc52fdaefe0b7682
Rafael Copat <rfc904@gmail.com>
2018-08-15 00:09:42 -0300

    New tuto

--- deck.rb
+++ deck.rb
@@ -1,6 +1,17 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+Squib::Deck.new(cards: 4, layout: 'layout.yml') do
+  background color: '#230602'
+  deck = xlsx file: 'bigger.xlsx'
+
+  text str: deck['Title'], layout: :Title
+
+  svg file: 'attack.svg', layout: :AttackIcon
+  text str: deck['Attack'], layout: :AttackText
+
+  svg file: 'defend.svg', layout: :DefendIcon
+  text str: deck['Defend'], layout: :DefendText
+
+  save_png prefix: 'bigger_'
 end
+
